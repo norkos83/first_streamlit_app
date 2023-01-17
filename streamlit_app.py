ddef321dec475691ed36619b6b69ecaba78ed4cf
norkos83 <31740393+norkos83@users.noreply.github.com>
2023-01-17 18:57:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog 
+'''
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the data into a dataframe
@@ -14,7 +15,7 @@ df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page so I Can see what I am working with 
 streamlit.write(df) 
-'''
+
 # put the first column into a list 
 color_list = df[0].values.tolist() 
 # print(color_list) 
