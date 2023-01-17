5391f6f62b7555a5214c06e79c05a0692810acde
norkos83 <31740393+norkos83@users.noreply.github.com>
2023-01-17 18:53:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,10 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the data into a dataframe
 df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with #
-streamlit.write(df) 
+
+# temp write the dataframe to the page so I Can see what I am working with 
+#streamlit.write(df) 
+
 # put the first column into a list 
 color_list = df[0].values.tolist() 
 # print(color_list) 
